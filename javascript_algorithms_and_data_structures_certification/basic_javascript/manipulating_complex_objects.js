947b481e698a4ae68b057785c98343aebc6efc33
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2023-12-29 11:20:39 +0800

    Create manipulating_complex_objects.js

---
+++ javascript_algorithms_and_data_structures_certification/basic_javascript/manipulating_complex_objects.js
@@ -0,0 +1,27 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
+
+myMusic.push({
+    "artist": "Paulo Oliver",
+    "title": "Free Code Camp",
+    "release_year": 2023,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  })
+
+console.log(myMusic);
